b022f42279d467b86fa4888c118a4454923b0b4b
whoant <vovanhoangtuan4.2@gmail.com>
2021-12-13 23:59:08 +0700

    Add sentry

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
-
 module.exports = {
     PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT: {
         token: process.env.JWT_TOKEN,
-        expires: process.env.JWT_EXPIRES
-    }
+        expires: process.env.JWT_EXPIRES,
+    },
+    SENTRY_URL: process.env.SENTRY_URL,
 };
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const morganBody = require('morgan-body');
 const routes = require('../api/routes');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
+
+const { SENTRY_URL } = require('../config');
 
 module.exports = (app) => {
     app.use(express.json());
+
+    Sentry.init({
+        dsn: SENTRY_URL,
+        integrations: [
+            new Sentry.Integrations.Http({ tracing: true }),
+            new Tracing.Integrations.Express({ app }),
+        ],
+        tracesSampleRate: 1.0,
+    });
+
+    app.use(Sentry.Handlers.requestHandler());
+    app.use(Sentry.Handlers.tracingHandler());
+
     morganBody(app);
 
     routes(app);
-    app.get('/', (req, res, next) => {
-        res.send('OK');
-    });
+
+    app.use(Sentry.Handlers.errorHandler());
     app.use((error, req, res, next) => {
         console.error('=== ERROR ===');
         console.error(error);
